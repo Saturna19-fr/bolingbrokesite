bab302036ca4641b306be3db7701e200a87fa1d2
Saturna19_fr <thibault.morez@outlook.com>
2025-03-21 22:59:43 +0100

    par pitié j'en peux plus

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 import { AppSidebar } from "@/components/app-sidebar"
 import { ChartAreaInteractive } from "@/components/chart-area-interactive"
 import { DataTable } from "@/components/data-table"
