bb49c19e91a206b154170614805dd443117ea5ff
Saturna19_fr <thibault.morez@outlook.com>
2025-03-21 20:40:04 +0100

    test encore zeubi jv pleurer

--- app/dashboard/debug/page.tsx
+++ app/dashboard/debug/page.tsx
@@ -26,19 +26,6 @@ export default async function Home() {
       )}
       {session && (
         <>
-      <p>
-      {!session ? "Not authenticated" : session.user.email}
-      </p>
-      <p>
-      {!session ? "No name available" : session.user.name}
-      </p>
-      <p>
-      {!session ? "No role available" : session.user.role}
-      </p>
-      <p>
-      {!session ? "No pole available" : session.user.pole}
-
-      </p> 
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism-okaidia.min.css" /> 
 
  <div className="max-w mx-auto mt-24 bg-gray-900 rounded-lg overflow-hidden"> 
