344dbda3843efe290c961250604b39414f002c05
Saturna19_fr <thibault.morez@outlook.com>
2025-03-21 20:26:57 +0100

    Removed fucking comments

--- app/page.tsx
+++ app/page.tsx
@@ -24,37 +24,6 @@ export default async function Home() {
       {session && (
         <Welcome />
       )}
-      {/* <Button onClick={signIn}>Sign In</Button> */}
-      {/* <Button onClick={giveAdmin}>Give Admin</Button> */}
-      {/*<p>
-      {!session ? "Not authenticated" : session.user.email}
-      </p>
-      <p>
-      {!session ? "No name available" : session.user.name}
-      </p>
-      <p>
-      {!session ? "No role available" : session.user.role}
-      </p>
-      <p>
-      {!session ? "No pole available" : session.user.pole}
-      </p> */}
-      {/* <code>{JSON.stringify(session, null, 2)}</code> */}
-      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism-okaidia.min.css" /> */}
-
-{/* <div className="max-w mx-auto mt-24 bg-gray-900 rounded-lg overflow-hidden"> */}
-
-  {/* <div className="p-2"> */}
-    {/* <div className="flex items-center justify-between"> */}
-      {/* <span className="text-gray-200 text-xl font-bold">Données:</span> */}
-    {/* </div> */}
-  {/* </div> */}
-
-  {/* <div className="px-3 py-">
-    <pre className="language-javascript">      <code>{JSON.stringify(session, null, 2)}</code>
-    </pre>
-  </div>
-</div> */}
-
     </main>
   );
 }
