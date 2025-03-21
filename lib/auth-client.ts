2cfc61a954b3f3175dd96df03ef2a0a359551ffb
Saturna19_fr <thibault.morez@outlook.com>
2025-03-21 20:59:03 +0100

    Edit variables d'env

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000", // the base url of your auth server
+    baseURL: "https://dataterminal.eu.saturna19.fr", // the base url of your auth server
     plugins: [
         adminClient()
     ]
