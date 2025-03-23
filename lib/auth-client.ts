93e679fd56976356da4b8bc1f41689b0f0474312
Saturna19_fr <thibault.morez@outlook.com>
2025-03-23 22:27:16 +0100

    changement du baseurl

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,8 @@ import { createAuthClient } from "better-auth/react"
 import { adminClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
-    // baseURL: "https://dataterminal.eu.saturna19.fr", // the base url of your auth server
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://dataterminal.eu.saturna19.fr", // the base url of your auth server
     plugins: [
         adminClient()
     ]
